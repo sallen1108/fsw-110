069c0ec88939d61847640ea6c1df98c8985b2404
Samantha Allen <samantha.allen@bryanuniversity.edu>
2020-08-02 22:21:15 -0500

    pap

---
+++ week5/pap1/pap1.js
@@ -0,0 +1,15 @@
+var input = document.getElementById("text-input")
+
+input.value = ""
+
+var input2 = document.getElementById("text-input2")
+
+input2.value = ""
+
+var input3 = document.getElementById("text-input3")
+
+input3.value = ""
+
+function alertTest() {
+    alert(input.value + input2.value + input3.value)
+}
